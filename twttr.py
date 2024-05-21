c333f6b66f60358cb6e5501b4506342f10f921ae
Helge <helgepeggen@web.de>
2024-05-21 11:43:11 +0200

    twttr komische lösung

--- twttr.py
+++ twttr.py
@@ -1,8 +1,11 @@
+# long version without loop
+# print("Output:", input("Input: ").replace("A", "").replace("a", "").replace("E", "").replace("e", "").replace("I", "").replace("i", "").replace("O", "").replace("o", "").replace("U", "").replace("u", ""))
+
 message = input("Input: ")
-message = message.replace("a", "")
 print("Output:", message)
 
 
+# List of Vowels to be replaced ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"]
 
 # if __name__ == "__main__":
 #     main()
