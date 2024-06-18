a73d7efe8f86f0e048dcea843d1c024b628cee85
Helge <helgepeggen@web.de>
2024-06-18 09:17:12 +0200

    twttr solved

--- twttr.py
+++ twttr.py
@@ -1,10 +1,7 @@
 vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"]
 
 message = input("Input: ")
+for char in message:
+    if char in vowels:
+        message = message.replace(char, "")
 print("Output:", message)
-.replace()
-
-# for char in str - if char not in vowels print char
-
-# if __name__ == "__main__":
-#     main()
