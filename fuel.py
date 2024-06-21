394958d8c1a06a771c379406ee5c82e78916974a
bawzboy <helgepeggen@web.de>
2024-06-21 15:38:28 +0200

    finished fuel.py

--- fuel.py
+++ fuel.py
@@ -1,16 +1,18 @@
-top, bottom = input("Fraction: ").split("/")
+while True:
+    top, bottom = input("Fraction: ").split("/")
 
-try:
-    result = round((int(top) / int(bottom)) * 100)
-    if result == 1:
-        print("E")
-    elif result == 100:
-        print("F")
-    else:
-        print(result, "%")
+    try:
+        result = round((int(top) / int(bottom)) * 100)
+        if result <= 1:
+            print("E")
+        elif result >= 99:
+            print("F")
+        else:
+            print(result, "%")
+        break
 
-except (ValueError, ZeroDivisionError):
-    print("An error occured!")
+    except (ValueError, ZeroDivisionError):
+        print("An error occured!")
 
 # if __name__ == "__main__":
 #     main()
