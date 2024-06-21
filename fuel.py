bd6844ce10d2f88bcc5fc95c12133c402cd3ca9e
Helge <helgepeggen@web.de>
2024-06-21 12:53:33 +0200

    try/except fuel.py

--- fuel.py
+++ fuel.py
@@ -1,9 +1,16 @@
 top, bottom = input("Fraction: ").split("/")
 
 try:
-    print(int(top) / int(bottom), "%")
+    result = round((int(top) / int(bottom)) * 100)
+    if result == 1:
+        print("E")
+    elif result == 100:
+        print("F")
+    else:
+        print(result, "%")
+
 except (ValueError, ZeroDivisionError):
-    ...
+    print("An error occured!")
 
 # if __name__ == "__main__":
 #     main()
