d421d9d0b11ca429323819daaada0882f1b519d4
bawzboy <helgepeggen@web.de>
2024-06-22 13:58:01 +0200

    finished taqueria.py

---
+++ taqueria.py
@@ -0,0 +1,24 @@
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+total = 0.00
+
+while True:
+    try:
+        order = input("Your order: ").title()
+        if order in menu:
+            total = total + menu[order]
+            print(f"$ {total:.2f}")
+
+    except EOFError:
+        print(f"\n$ {total:.2f}")
+        break
