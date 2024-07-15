3fbc7cb38b3d5c4da432052daae064a89dd70273
bawzboy <helgepeggen@web.de>
2024-07-15 18:01:58 +0200

    -

---
+++ Libraries/Figlet.py
@@ -0,0 +1,13 @@
+from pyfiglet import Figlet
+import sys
+import random
+
+
+if len(sys.argv) == 1:
+    ...
+elif len(sys.argv) == 3:
+    ...
+elif len(sys.argv) == 2:
+    sys.exit("Please enter 0 or 2 arguments")
+else:
+    sys.exit("Too many arguments")
