cadb1c119cfd34f1f75c4902aa0c26304fd1075b
Helge <helgepeggen@web.de>
2024-07-16 12:29:33 +0200

    -

--- Libraries/Figlet.py
+++ Libraries/Figlet.py
@@ -3,11 +3,13 @@ import sys
 import random
 
 
-if len(sys.argv) == 1:
+if len(sys.argv) == 1:  # 0 arguments
     ...
-elif len(sys.argv) == 3:
+elif len(sys.argv) == 3: # 2 arguments
     ...
 elif len(sys.argv) == 2:
     sys.exit("Please enter 0 or 2 arguments")
 else:
     sys.exit("Too many arguments")
+
+message = input("Your message: ")
