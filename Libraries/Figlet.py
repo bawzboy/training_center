242128ae527d66d0b0bf98346d68a48ae2c33e32
Helge <helgepeggen@web.de>
2024-07-18 11:47:18 +0200

    Finish Figlet.py

--- Libraries/Figlet.py
+++ Libraries/Figlet.py
@@ -1,16 +1,20 @@
 from pyfiglet import Figlet
 import sys
-import random
+from random import randint
 
-if len(sys.argv) == 1:
-    ...
-if len(sys.argv) == 1:  # 0 arguments
-    ...
-elif len(sys.argv) == 3: # 2 arguments
-    ...
+figlet = Figlet()
+list_of_fonts = figlet.getFonts() # 549 fonts
+
+if len(sys.argv) == 1:  # = 0 arguments
+    random_font = randint(0, len(list_of_fonts))
+    figlet.setFont(font = list_of_fonts[random_font])
+elif len(sys.argv) == 3:  # = 2 arguments
+    if sys.argv[1] == "-f" or "--font":
+        figlet.setFont(font = sys.argv[2])
 elif len(sys.argv) == 2:
     sys.exit("Please enter 0 or 2 arguments")
 else:
     sys.exit("Too many arguments")
 
 message = input("Your message: ")
+print(figlet.renderText(message))
