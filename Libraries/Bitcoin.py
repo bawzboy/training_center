ead1a9dc81445aea5f83efad9fe03f3ba97a1cbe
bawzboy <helgepeggen@web.de>
2024-07-21 15:50:33 +0200

    Finish Bitcoin.py

--- Libraries/Bitcoin.py
+++ Libraries/Bitcoin.py
@@ -1,13 +1,21 @@
 import sys
 import requests
 
+if len(sys.argv) != 2:
+    sys.exit("Missing command-line argument")
 
+try:
+    bitcoin = float(sys.argv[1])
+except ValueError:
+    sys.exit("Command-line argument is not a number")
 
+try:
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
+except requests.RequestException:
+    print("Error")
 
+rate = float(response["bpi"]["USD"]["rate"].replace(",", ""))
 
-# try:
-#     ...
-# except requests.RequestException:
-#     ...
+amount = bitcoin * rate
 
-# print(f"${amount:,.4f}") format amount to 4 decimal places
+print(f"${amount:,.4f}")
