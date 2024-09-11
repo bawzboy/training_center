f7893b5723e55661b43472054207903138db2e00
Helge <helgepeggen@web.de>
2024-09-11 11:09:43 +0200

    finish scourgify.py add shirt.py

--- FileIO/Scourgify/scourgify.py
+++ FileIO/Scourgify/scourgify.py
@@ -8,31 +8,19 @@ def main():
     try:
         with open(sys.argv[1]) as read_file:
             reader = csv.DictReader(read_file)
-        for row in reader:
-            last_name, first_name = row["name"].split(", ")
+
             with open(sys.argv[2], "w") as write_file:
                 fieldnames = ["first", "last", "house"]
                 writer = csv.DictWriter(write_file, fieldnames = fieldnames)
 
-                writer.writerow({"first": first_name, "last": last_name, "house": row["house"]})         
+                writer.writeheader()
 
-
-        # read_csv(sys.argv[1])
-        # write_csv(sys.argv[2])
+                for row in reader:
+                    last_name, first_name = row["name"].split(", ")
+                    writer.writerow({"first": first_name, "last": last_name, "house": row["house"]})         
 
     except FileNotFoundError:
         sys.exit("File not found")
-
-# def read_csv(file_to_read):
-#     with open(file_to_read) as file:
-#         reader = csv.DictReader(file)
-#     for row in reader:
-#         last_name, first_name = row["name"].split(", ")
-#         return last_name, first_name, row["house"] 
-    
-# def write_csv(file_to_write):
-#     with open(file_to_write, "w") as file:
-#         fieldnames = ["first", "last", "house"]
 
 if __name__ == "__main__":
     main()
