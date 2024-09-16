a6ca35df94e5bb698a75c5e6d54000075527fe99
Helge <helgepeggen@web.de>
2024-09-16 11:24:31 +0200

    finish shirt.py

--- FileIO/Shirt/shirt.py
+++ FileIO/Shirt/shirt.py
@@ -1,10 +1,32 @@
 import sys
 import os
-from PIL import image
+from PIL import Image, ImageOps
+from posixpath import splitext
+
+file_formats = (".jpeg", ".jpg", ".png")
+shirt = Image.open("shirt.png")
+shirt_size = shirt.size
+
+if len(sys.argv) != 3:
+    sys.exit("Wrong number of arguments")
+
+elif not (sys.argv[1].lower().endswith(file_formats) and 
+          sys.argv[2].lower().endswith(file_formats)):
+    sys.exit("Wrong file format")
+
+elif splitext(sys.argv[1])[1] != splitext(sys.argv[2])[1]:
+    sys.exit("Not same file format")
+
+def main():
+    try:
+        picture = Image.open(sys.argv[1])
+        picture = ImageOps.fit(picture, size = shirt_size)
+        picture.paste(shirt, shirt)
+        picture.save(sys.argv[2])
+    
+    except FileNotFoundError:
+        ("File not found")
 
 
-
-
-# shirt = Image.open("shirt.png")
-# size = shirt.size
-# photo.paste(shirt, shirt)
+if __name__ == "__main__":
+    main()
