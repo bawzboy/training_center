8ebd4d829a5f747e157eac326df8add6aaa9ed45
bawzboy <helgepeggen@web.de>
2024-09-23 20:35:33 +0200

    Add seasons.py

---
+++ OOP/Seasons/seasons.py
@@ -0,0 +1,13 @@
+from datetime import date
+import inflect
+
+def main():
+    p = inflect.engine()
+
+
+if __name__ == "__main__":
+    main()
+
+# inflect:
+# words = p.number_to_words(1234)
+# # "one thousand, two hundred and thirty-four"
