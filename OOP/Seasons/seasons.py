97d5e270e829ca371f6dc05eb598575e82558a05
Helge <helgepeggen@web.de>
2024-09-24 11:17:14 +0200

    Finish seasons.py

--- OOP/Seasons/seasons.py
+++ OOP/Seasons/seasons.py
@@ -2,12 +2,15 @@ from datetime import date
 import inflect
 
 def main():
-    p = inflect.engine()
+    print(minutes_since(input("Your Birthdate: ")))
 
+def minutes_since(birthdate):
+    year, month, day = birthdate.split("-")
+    birthdate = date(int(year), int(month), int(day))
+    days_since = date.today() - birthdate
+    minutes_since = days_since.days * 24 * 60
+    p = inflect.engine()
+    return p.number_to_words(minutes_since, andword="")
 
 if __name__ == "__main__":
     main()
-
-# inflect:
-# words = p.number_to_words(1234)
-# # "one thousand, two hundred and thirty-four"
